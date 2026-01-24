cae1d786fa4fdda230751245c6df6a9e21d1e7f0
Jake Cawfield <jake@Jakes-MacBook-Pro.local>
2026-01-24 18:17:20 +1100

    Fix blank page on new tab issue by switching to HashRouter

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { DiscoveryCall } from './pages/DiscoveryCall';
 import { ScrollToTop } from './components/ScrollToTop';
